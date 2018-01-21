25c65237fb58396fa6c1d52d7d9ab8fa8025a974
Lily <Lily@Kieying-Lily>
2018-01-20 22:31:30 -0800

    :)

--- print2Dface/src/print2Dface/Face.java
+++ print2Dface/src/print2Dface/Face.java
@@ -1,5 +1,26 @@
 package print2Dface;
 
 public class Face {
-
+	public static void main(String[] args)
+	{
+		String[][] face = new String[5][5];
+		
+		face[1][1]= "O";
+		face[1][3]= "O";
+		face[2][2]= "C";
+		face[3][1]= ")";
+		face[3][2]= "-";
+		face[3][3]= "(";
+		
+		for(String[] i: face) {
+			for(String j : i) {
+			    if(j == null) {
+					System.out.print(" ");
+			    }
+			    else
+			    	System.out.print(j);
+			}
+			System.out.println();
+		}
+	}
 }
